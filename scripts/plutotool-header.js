659b62be7fdf44380677ea581277f75bd0b85f05
Marco Giammarini <m.giammarini@warcomeb.it>
2021-10-22 22:41:55 +0200

    Start project...

---
+++ scripts/plutotool-header.js
@@ -0,0 +1,10 @@
+document.write(
+'<header class="p-2 bg-dark text-white shadow">\
+    <div class="container">\
+        <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">\
+            <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none navbar-brand">\
+                PlutoTool 1.0\
+            </a>\
+        </div>\
+    </div>\
+</header>');
