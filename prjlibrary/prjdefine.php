297ccdb4921a4cd749f4900d1b18b823d6c16a8a
Marco Giammarini <m.giammarini@warcomeb.it>
2022-08-30 06:47:23 +0200

    .

---
+++ prjlibrary/prjdefine.php
@@ -0,0 +1,57 @@
+<?php
+/*
+ * This file is part of PlutoTool!
+ * Copyright (C) 2022 Marco Giammarini
+ *
+ * Author(s):
+ *  Marco Giammarini <m.giammarini@warcomeb.it>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>
+ */
+
+define("PRJ_NAME"                   , "PlutoTool");
+define("PRJ_VERSION"                , "1.0");
+define("PRJ_COPYRIGHT"              , "Marco Giammarini");
+define("PRJ_YEAR"                   , "2022");
+
+// Database Table's name
+define("PRJ_DB_TABLE_USER"          , "user");
+define("PRJ_DB_TABLE_SESSION"       , "usersession");
+// define("PRJ_DB_TABLE_ACCOUNT"       , "account");
+// define("PRJ_DB_TABLE_ACCOUNT_TYPE"  , "accounttype");
+define("PRJ_DB_TABLE_PAYEE"         , "payee");
+define("PRJ_DB_TABLE_PAYEE_TYPE"    , "payeetype");
+// define("PRJ_DB_TABLE_WORKORDER"     , "workorder");
+// define("PRJ_DB_TABLE_CATEGORY"      , "category");
+// define("PRJ_DB_TABLE_SUBCATEGORY"   , "subcategory");
+// define("PRJ_DB_TABLE_TRANSACTION"   , "transaction");
+
+// Code start char
+define("PRJ_CODE_USER"              , "U");
+define("PRJ_CODE_ACCOUNT"           , "A");
+define("PRJ_CODE_PAYEE"             , "P");
+define("PRJ_CODE_WORKORDER"         , "W");
+
+// Element code length
+define("PRJ_CODE_LENGTH"            , 5);
+
+// Category types
+define("PRJ_CATEGORY_TYPE_INPUT"    , "i");
+define("PRJ_CATEGORY_TYPE_OUTPUT"   , "o");
+define("PRJ_CATEGORY_TYPE_NEUTRAL"  , "n");
+define("PRJ_CATEGORY_TYPE_ALWAYS"   , "a");
+
+
+// Login define
+define("LOGIN_EXPIRE_SESSION"       , 3600);
